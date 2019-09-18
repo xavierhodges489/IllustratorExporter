a5014c23d1ffc5d4d47f8bc91ad79a5b81901840
Xavier Hodges <xavierhodges489@gmail.com>
2019-09-18 18:16:47 -0400

    Reorganized GUI and added ability to change artboard names for export

--- exportTemplate.jsx
+++ exportTemplate.jsx
@@ -5,8 +5,9 @@ var prefix;
 var suffix;
 var pdfArtboards = [];
 var jpgArtboards = [];
+var names = [];
   
-function exportPNG24(destFolder, artboard) {  
+function exportPNG24(destFolder, name, artboard) {  
 
     app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
 
@@ -16,20 +17,20 @@ function exportPNG24(destFolder, artboard) {
     
     
     docRef.artboards.setActiveArtboardIndex( artboard );
-    var fileName = prefix.text + docRef.artboards[artboard].name + suffix.text + ".png";
+    var fileName = prefix.text + name.text + suffix.text + ".png";
     var fileSpec = File(destFolder + "/" + fileName);
     docRef.exportFile ( fileSpec, ExportType.PNG24, opts );
     
     app.userInteractionLevel = UserInteractionLevel.DISPLAYALERTS;  
 }
 
-function exportPDF(destFolder, artboard) {  
+function exportPDF(destFolder, name, artboard) {  
 
     var pdfSaveOptions = new PDFSaveOptions();
     pdfSaveOptions.artboardRange = artboard;
     pdfSaveOptions.pDFPreset = app.PDFPresetsList[3];
 
-    var fileName = prefix.text + docRef.artboards[artboard-1].name + suffix.text + ".pdf";
+    var fileName = prefix.text + name.text + suffix.text + ".pdf";
 
     var destFile = File(destFolder + "/" + fileName);
 	docRef.saveAs (destFile,  pdfSaveOptions);	 
@@ -43,13 +44,13 @@ function save(){
 
         for ( ii = 0; ii < pdfArtboards.length; ii++ ) {
             if(pdfArtboards[ii].value==true){
-                exportPDF(destFolder, ii+1);
+                exportPDF(destFolder, names[ii], ii+1);
             }
         }
 
         for ( ii = 0; ii < jpgArtboards.length; ii++ ) {
             if(jpgArtboards[ii].value==true){
-                exportPNG24(destFolder, ii);
+                exportPNG24(destFolder, names[ii], ii);
             }
         }
     }
@@ -59,35 +60,53 @@ function openDialog() {
 
     var dlg = new Window("dialog", "Artboard Exporter");
 
-    var pdfRow = dlg.add('group', undefined, '');
-    pdfRow.oreintation = 'row';
+    var checkRow = dlg.add('group', undefined, '');
+    checkRow.orientation = 'row';
+    checkRow.alignment = 'left';
 
-    var jpgRow = dlg.add('group', undefined, '');
-    jpgRow.oreintation = 'row';
-        
-    pdfRow.add("statictext", undefined, "Save as PDF: ");
-    jpgRow.add("statictext", undefined, "Save as JPG: ");
+
+    var checkColumn = checkRow.add('group', undefined, '');
+    checkColumn.orientation = "column";
+
+    checkColumn.add("statictext", undefined, "Artboard: ").alignment = 'right';
+    
+    checkColumn.add("statictext", undefined, "Export as PDF: ").alignment = 'right';
+    
+    checkColumn.add("statictext", undefined, "Export as JPG: ").alignment = 'right';
+    
+    checkColumn.add("statictext", undefined, "Name: ").alignment = 'right';
+    
 
     for(var i=0; i<docRef.artboards.length; i++){
 
         var artboard = docRef.artboards[i].name;
 
-        var pdfCheck =  pdfRow.add("checkbox", undefined, artboard);
+        checkColumn = checkRow.add('group', undefined, '');
+        checkColumn.orientation = "column";
+
+        checkColumn.add("statictext", undefined, (i+1));
+
+        var pdfCheck =  checkColumn.add("checkbox");
         if(artboard==="8.5x11" || artboard==="11x17" || artboard==="24x36"){
             pdfCheck.value = true;
         }
 
-        var jpgCheck = jpgRow.add("checkbox", undefined, artboard);
+        var jpgCheck = checkColumn.add("checkbox");
         if(artboard==="1024x512" || artboard==="1140x325" || artboard==="1080x1920" || artboard==="1200x900"){
             jpgCheck.value = true;
         }
 
+        var name = checkColumn.add("edittext", undefined, artboard);
+        name.characters = 8;
+
         pdfArtboards.push(pdfCheck);
         jpgArtboards.push(jpgCheck);
+        names.push(name);
     }
 
     var ixRow = dlg.add('group', undefined, '');
-    ixRow.oreintation = 'row';
+    ixRow.orientation = 'row';
+    ixRow.alignment = 'left';
 
     ixRow.add("statictext", undefined, "Prefix: ");
     prefix = ixRow.add("edittext", undefined, docName + "_");
@@ -102,6 +121,7 @@ function openDialog() {
     var buttonRow;
     buttonRow = dlg.add('group', undefined, ''); 
     buttonRow.orientation = 'row';
+    buttonRow.alignment = 'right';
     
     var cancelBtn = buttonRow.add('button', undefined, 'Cancel', {name:'cancel'});
 		cancelBtn.onClick = function() { dlg.close() };
